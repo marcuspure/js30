6b71571b7848ce323062dfd55d389cdd15a689b1
marcus <https://github.com/marcuspure>
2021-03-24 16:12:39 +0800

    timer

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,46 @@
+let timer;
+const buttons = document.querySelectorAll('.timer__controls > button');
+const timeLeft = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
+
+
+const startTimer = function(sec) {
+   clearInterval(timer);
+  const now = Date.now();
+  const end = now + sec * 1000;
+  //倒數計時
+   timer = setInterval(function() {
+    const secLeft = Math.floor((end - Date.now()) / 1000);
+    if (secLeft >= 0 ){
+      const disMin = Math.floor(secLeft / 60);
+      const disSec = secLeft % 60;
+      timeLeft.innerText = `${disMin} : ${disSec}`;
+    }else
+     clearInterval(timer);
+    
+  },16);//每秒更新60次
+  //顯示最後時間
+  const endDate = new Date(end);
+  let hour = endDate.getHours();
+  let min = endDate.getMinutes();
+  endTime.innerText = `back to ${hour}:${min}`;
+
+};
+
+function setTimer(){
+const sec = parseInt(this.dataset.time);
+startTimer(sec);
+};
+
+
+buttons.forEach(button => button.addEventListener("click", setTimer));
+
+document.querySelector('#custom').addEventListener("submit", function(e){
+  e.preventDefault();
+  const value = parseInt(this.minutes.value); 
+  if(value){
+    startTimer(value * 60);
+    this.reset();
+  }
+});
